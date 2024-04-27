e5e2533d3a033634c9a221730323b877cbcc2a3f
pacarti <a.paciorkowski@protonmail.com>
2024-04-27 21:56:19 +0200

    Step3 - added distingushing the text with given phrase from others

--- xdaSearch.py
+++ xdaSearch.py
@@ -9,6 +9,8 @@ headers = {
 
 subforumLink = 'https://xdaforums.com/f/redmi-9-power-9t.12055/page-9999999' # page numer so that it goes to the last page
 
+phrase = 'wifi'
+
 res = requests.get(subforumLink, headers=headers)
 
 # print(res.raise_for_status())
@@ -18,5 +20,17 @@ subforumSoup = bs4.BeautifulSoup(res.text, 'html.parser')
 
 linkElems = subforumSoup.select('a[data-xf-init="preview-tooltip"]')
 
+# Search the selected phrase from this page:
+
 for linkElem in linkElems:
-    print(linkElem.getText())
+    linkText = linkElem.getText()
+    if phrase in linkText:
+        print(linkText)
+    # print(linkElem.getText())
+
+
+# TODO: Search the selected phrase from all pages
+
+# TODO: Open the webbrowser on distinguished websites 
+
+# TODO: Change static link and phrase to dynamic
