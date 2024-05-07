a58c9b1c7ea31383a7e1ad31abee3bf98fdc9d8a
pacarti <a.paciorkowski@protonmail.com>
2024-05-07 23:44:09 +0200

    added distinguishing of chosen phrase from all the subpages

--- xdaSearch.py
+++ xdaSearch.py
@@ -19,8 +19,8 @@ headers = {
 
 # 1st phase - constant link, 2nd phase - link from argument
 
-# subforumLink = 'https://xdaforums.com/f/redmi-9-power-9t.12055/page-9999999' # page numer so that it goes to the last page
-subforumLink = 'https://xdaforums.com/f/redmi-9-power-9t.12055/page-2' # page numer so that it goes to the last page
+subforumLink = 'https://xdaforums.com/f/redmi-9-power-9t.12055/page-9999999' # page numer so that it goes to the last page
+# subforumLink = 'https://xdaforums.com/f/redmi-9-power-9t.12055/page-2' # page numer so that it goes to the last page
 
 res = requests.get(subforumLink, headers=headers)
 
@@ -40,10 +40,10 @@ linkElems = subforumSoup.select('a[data-xf-init="preview-tooltip"]') # <-- this 
 while not currentUrl.endswith('page-1'):
 
     for linkElem in linkElems:
-        '''linkText = linkElem.getText()
-        if phrase in linkText:
-            print(linkText)'''
-        print(linkElem.getText())
+        # linkText = linkElem.getText()
+        if phrase in linkElem.getText():
+            print(linkElem.getText())
+        # print(linkElem.getText())
 
     if currentUrl.endswith('page-1') == True:
         break
